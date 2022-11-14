258c71c402239b34b817b6bbf749facf4cd6449a
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-14 15:31:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main() {
 float a=0, b=0, x=0, y=0, z=0;
 cin>>a>>b>>x>>y;
 if (x<0 && y>0) {z=a*x-b*y;}
-    else if (x>=0 && y<=0) {z=a*x^2-b*y;}
-        else {z=a*x-b*y^2;}
+    else if (x>=0 && y<=0) {z=a*(x*x)-b*y;}
+        else {z=a*x-b*(y*y);}
 cout<<z<<endl;
    return 0;
 }
