4286173720ff8249e047f02dbffa31c7e08d31f2
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-14 15:23:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main() {
+   int a=0, b=0, x=0, y=0, z=0;
+cin>>a>>b>>x>>y;
+if (x<0 && y>0) {z=a*x-b*y;}
+    else if (x>=0 && y<=0) {z=a*x^2-b*y;}
+        else {z=a*x-b*y^2;}
+cout<<z<<endl;
    return 0;
 }
